d9a89e933e2a77f7742916f5edd608ea552a296b
joon <104882985+joonhyoo@users.noreply.github.com>
2026-09-17 05:56:14 +0000

    update: deprecated z import due to migration to new AstroJS

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,22 +1,23 @@
-import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+import { glob } from "astro/loaders";
 
 const portfolio = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/content/portfolio' }),
+  loader: glob({ pattern: "**/*.md", base: "./src/content/portfolio" }),
   schema: z.object({
     title: z.string(),
     thumbnails: z.array(z.string()).default([]),
     summary: z.string(),
-    github: z.string().url().default(''),
-    live: z.string().url().default(''),
+    github: z.url().default(""),
+    live: z.url().default(""),
     tags: z.array(z.string()).default([]),
-    kind: z.enum(['personal project', 'university coursework']).optional(),
+    kind: z.enum(["personal project", "university coursework"]).optional(),
     id: z.number(),
   }),
 });
 
 const blog = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/content/blog' }),
+  loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
